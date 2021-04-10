a17e7640dfc2be85159a2bd98ed740f32e5a82ae
daviddicken <davidfromseattle@gmail.com>
2021-04-09 20:42:11 -0700

    completed Alien Dictionary challenge

---
+++ C-Sharp/AlienDictionary/AlienDictionary/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace AlienDictionary
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      string[] inputArr = new string[] { "hell", "hello" };
+      string order = "hlabcdefgijkmnopqrstuvwxyz";
+
+      Console.WriteLine(IsAlienSorted(inputArr, order));
+
+
+    }
+    public static bool IsAlienSorted(string[] words, string order)
+    {
+
+      for (int i = 0; i < words.Length - 1; i++)
+      {
+        string word1 = words[i];
+        string word2 = words[i + 1];
+        int j = 0;
+        while (order.IndexOf(word2[j]) == order.IndexOf(word1[j]))
+        {
+          j++;
+          if (j == word2.Length && j < word1.Length) return false;
+          if (j == word2.Length && j == word1.Length) break;
+          if (j < word2.Length && j == word1.Length) break;
+        }
+        if(j < word1.Length && j < word2.Length)
+        {
+          int first = order.IndexOf(word1[j]);
+          int second = order.IndexOf(word2[j]);
+          if (first > second) return false;
+
+        }
+      }
+      return true;
+    }
+
+  }
+}
---
+++ C-Sharp/AlienDictionary/README.md
@@ -0,0 +1,27 @@
+# Verifying an Alien Dictionary
+Date: 4/9/2021
+Site: LeetCode
+
+# Challenge
+In an alien language, surprisingly they also use english lowercase letters, but possibly in a different order.  
+The order of the alphabet is some permutation of lowercase letters.
+
+Given a sequence of words written in the alien language, and the order of the alphabet, return true if and only if  
+the given words are sorted lexicographicaly in this alien language.
+
+# Examples
+```C#
+Input: words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"
+Output: true
+Explanation: As 'h' comes before 'l' in this language, then the sequence is sorted.
+```
+```Java
+Input: words = ["word","world","row"], order = "worldabcefghijkmnpqstuvxyz"
+Output: false
+Explanation: As 'd' comes after 'l' in this language, then words[0] > words[1], hence the sequence is unsorted.
+```
+```
+Input: words = ["apple","app"], order = "abcdefghijklmnopqrstuvwxyz"
+Output: false
+Explanation: The first three characters "app" match, and the second string is shorter (in size.)
+```
